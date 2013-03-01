a1b9954a11dfbc93a3d00b49cf3190942e3a9866
Ian Buchanan <ian.buchanan@versionone.com>
2013-03-01 14:49:11 -0500

    use common build script, stub AssemblyInfo.cs files, and add AssemblyInfo.cs to .gitignore since they are generated by build script

--- VersionOneTFSServerConfig/Properties/AssemblyInfo.cs
+++ VersionOneTFSServerConfig/Properties/AssemblyInfo.cs
@@ -1,34 +1,17 @@
-﻿using System.Reflection;
+// Stub file to be overwritten by build.sh
+
+using System;
+using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("VersionOne TFS Listener Config")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("See https://github.com/versionone/V1TFS/wiki")]
+
+[assembly: AssemblyProduct("VersionOneTFSServerConfig")]
+[assembly: AssemblyTitle("VersionOneTFSServerConfig")]
+[assembly: AssemblyDescription("VersionOne Team Foundation Server Integration Release Build")]
 [assembly: AssemblyCompany("VersionOne, Inc.")]
-[assembly: AssemblyProduct("VersionOne TFS Listener Config")]
-[assembly: AssemblyCopyright("Copyright © 2008, VersionOne, Inc. All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fdafac52-45a2-4a1b-937e-db6fb8adbe4d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("8.2.0.0")]
-[assembly: AssemblyFileVersion("8.2.0.0")]
-[assembly: AssemblyInformationalVersion("8.2")]
+[assembly: AssemblyCopyright("Copyright 2008-2013, VersionOne, Inc., Licensed under modified BSD.")]
