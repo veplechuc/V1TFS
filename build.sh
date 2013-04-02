2a9e0b8ad2e1bae6989481289a63ad23bf42b386
codeEmitter <greg.johnson@versionone.com>
2013-04-02 11:52:04 -0400

    Forcing nuget package restore through the nuget.targets config.

--- build.sh
+++ build.sh
@@ -169,6 +169,5 @@ done
 # ---- Build solution using msbuild -------------------------------------------
 
 WIN_SIGNING_KEY="`winpath "$SIGNING_KEY"`"
-MSBuild.exe $SOLUTION_FILE -p:SignAssembly=$SIGN_ASSEMBLY -p:AssemblyOriginatorKeyFile=$WIN_SIGNING_KEY -p:RequireRestoreConsent=false
-
+MSBuild.exe $SOLUTION_FILE -p:SignAssembly=$SIGN_ASSEMBLY -p:AssemblyOriginatorKeyFile=$WIN_SIGNING_KEY
 
