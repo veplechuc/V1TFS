162994afaedc5bdd7321ba4f64eb437ab9dfcfc2
codeEmitter <greg.johnson@versionone.com>
2013-04-08 16:35:59 -0400

    Implementing structures to store and iterate over keys to be stored in appSettings in the web.config.

--- VersionOneTFSServer.Tests/ConfigurationProviderSpecs.cs
+++ VersionOneTFSServer.Tests/ConfigurationProviderSpecs.cs
@@ -7,6 +7,27 @@ namespace VersionOneTFSServer.Tests
 {
     public class ConfigurationProviderSpecs : nspec
     {
+
+        void given_app_setting_keys_are_needed_for_recursion()
+        {
+
+            context["when i request a collection of app setting keys"] = () =>
+                {
+                    it["then i receive a collection to iterate over"] = () =>
+                        {
+                            var keys = AppSettingKeysCollection.Retrieve();
+                            keys.Count.should_not_be(0);
+                            foreach (var key in keys)
+                            {
+                                key.should_not_be(null);
+                                key.should_not_be(string.Empty);
+                                key.Value.should_not_be(null);
+                                key.Value.should_not_be(string.Empty);
+                            }
+                        };
+                };
+        }
+
         void given_configuration_settings_are_not_yet_saved()
         {
 
--- VersionOneTFSServer/Providers/ConfigurationProvider.cs
+++ VersionOneTFSServer/Providers/ConfigurationProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Policy;
+﻿using System.Collections.Generic;
+using System.Security.Policy;
 using VersionOne.TFS2010.DataLayer.Interfaces;
 using VersionOneTFSServer.Adapters;
 using VersionOneTFSServer.Interfaces;
@@ -6,8 +7,47 @@ using VersionOneTFSServer.Interfaces;
 namespace VersionOneTFSServer.Providers
 {
 
+    public struct AppSettingKeys
+    {
+        public const string Integrated = "Integrated";
+        public const string V1Url = "V1Url";
+        public const string UserName = "UserName";
+        public const string Password = "Password";
+        public const string ProxyEnabled = "ProxyEnabled";
+        public const string ProxyUrl = "ProxyUrl";
+        public const string ProxyDomain = "ProxyDomain";
+        public const string ProxyUserName = "ProxyUserName";
+        public const string ProxyPassword = "ProxyPassword";
+    }
+
+    /// <summary>
+    /// Provided access to a collectionization of the AppSettingsKeys structure.
+    /// </summary>
+    public static class AppSettingKeysCollection
+    {
+
+        public static Dictionary<string, string> Retrieve()
+        {
+
+            var keysCollection = new Dictionary<string, string>();
+            var instance = new AppSettingKeys();
+            foreach (var field in typeof(AppSettingKeys).GetFields())
+            {
+                if (field.IsPrivate) continue;
+                keysCollection.Add(field.Name, field.GetValue(instance) as string);
+            }
+
+            return keysCollection;
+
+        }
+    }
+
+    /// <summary>
+    /// Provides configuration information for the 
+    /// </summary>
     public class ConfigurationProvider : IConfigurationProvider
     {
+
         public bool Integrated { get; private set; }
         public Url V1Url { get; private set; }
         public string Username { get; private set; }
@@ -16,6 +56,8 @@ namespace VersionOneTFSServer.Providers
 
         public void ResetDefaults()
         {
+
+            
             WebConfigurationAdapter.ClearAllAppSettings();
         }
     }
