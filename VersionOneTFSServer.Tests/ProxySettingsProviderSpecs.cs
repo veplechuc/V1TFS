1f25b58bb25ef81c8b1e575eabda0f9d00d2d21e
codeEmitter <greg.johnson@versionone.com>
2013-04-19 14:57:49 -0400

    Testing that values saved and retrieved are equal.

--- VersionOneTFSServer.Tests/ProxySettingsProviderSpecs.cs
+++ VersionOneTFSServer.Tests/ProxySettingsProviderSpecs.cs
@@ -1,16 +1,19 @@
-﻿using Integrations.Core.Adapters;
+﻿using System.Collections.Generic;
+using Integrations.Core.Adapters;
 using Integrations.Core.Interfaces;
 using NSpec;
+using VersionOneTFSServer.Collections;
 using VersionOneTFSServer.Providers;
 
 namespace VersionOneTFSServer.Tests
 {
     public class ProxySettingsProviderSpecs : nspec
     {
+
         private IProxyConnectionSettings _target = null;
         private IProxyConnectionSettings _defaults = null;
 
-        private void given_versionOne_specific_settings_are_not_yet_saved_in_the_web_config()
+        public void given_versionOne_specific_settings_are_not_yet_saved_in_the_web_config()
         {
             before = () =>
             {
@@ -30,6 +33,45 @@ namespace VersionOneTFSServer.Tests
                     _target.Password.should_be(_defaults.Password);
                 };
             };
+        }
+
+        public void given_versionOne_specific_settings_are_saved_in_the_web_config()
+        {
+
+            const string proxyIsEnabled = "false";
+            const string proxyDomain = "DOMAIN5000";
+            const string proxyUrl = "http://myProxyUrl:9192/login/";
+            const string proxyUserName = "ProxyAdmin1";
+            const string proxyPassword = "ProxyPass1";
+
+            before = () =>
+                {
+                    _target = new ProxySettingsProvider();
+                    _defaults = new DefaultProxySettingsProvider();
+                    WebConfigurationAdapter.ClearAllAppSettings();
+                    
+                    WebConfigurationAdapter.SaveAppSettings(new Dictionary<string, string>()
+                        {
+                            {AppSettingKeys.ProxyIsEnabled, proxyIsEnabled},
+                            {AppSettingKeys.ProxyDomain, proxyDomain},
+                            {AppSettingKeys.ProxyUrl, proxyUrl},
+                            {AppSettingKeys.ProxyUserName, proxyUserName},
+                            {AppSettingKeys.ProxyPassword, proxyPassword}
+                        });
+                };
+
+            context["when i retrieve versionone specific settings"] = () =>
+                {
+                    it["then the saved values are returned"] = () =>
+                        {
+                            _target.ProxyIsEnabled.should_be(bool.Parse(proxyIsEnabled));
+                            _target.Domain.should_be(proxyDomain);
+                            _target.Url.ToString().should_be(proxyUrl.ToLower()); //uri object stores url string in all lowercase
+                            _target.Username.should_be(proxyUserName);
+                            _target.Password.should_be(proxyPassword);
+                        };
+                };
+
         }
 
     }
