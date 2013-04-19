f020d1831c9872e5d46ccee5fb68a17d3a6b8e0c
codeEmitter <greg.johnson@versionone.com>
2013-04-19 10:40:25 -0400

    Adding default proxy settings provider.

---
+++ VersionOneTFSServer/Providers/DefaultProxySettingsProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Integrations.Core.Interfaces;
+
+namespace VersionOneTFSServer.Providers
+{
+    public class DefaultProxySettingsProvider : IProxyConnectionSettings
+    {
+        public bool UseProxy
+        {
+            get { return false; }
+        }
+        public string Url
+        {
+            get { return "http://192.168.1.1/"; }
+        }
+        public string Domain
+        {
+            get { return "AD"; }
+        }
+        public string Username
+        {
+            get { return "Administrator"; }
+        }
+        public string Password
+        {
+            get { return "123456"; }
+        }
+    }
+}
