0a815435c0ec291f0162a6a2ba5c0231bb963f97
codeEmitter <greg.johnson@versionone.com>
2013-04-23 09:29:01 -0400

    Adding new tests assembly to bash script.

--- tests.sh
+++ tests.sh
@@ -2,3 +2,4 @@
 ./packages/nspec.0.9.64/tools/NSpecRunner.exe Integration.Core.Tests/bin/Integration.Core.Tests.dll
 ./packages/nspec.0.9.64/tools/NSpecRunner.exe VersionOneTFSServer.Tests/bin/VersionOneTFSServer.Tests.dll
 ./packages/nspec.0.9.64/tools/NSpecRunner.exe VersionOne.TFS2010.DataLayer.Tests/bin/VersionOne.TFS2010.DataLayer.Tests.dll
+./packages/nspec.0.9.64/tools/NSpecRunner.exe VersionOneTFSServerConfig.Tests/bin/VersionOneTFSServerConfig.Tests.dll
