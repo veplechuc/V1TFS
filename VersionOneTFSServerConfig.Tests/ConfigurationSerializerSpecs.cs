841f488f5e596a4ff41d7b62e4468c6b4dad88ac
codeEmitter <greg.johnson@versionone.com>
2013-04-23 10:26:39 -0400

    Framing out more tests for the configuration serializer.

--- VersionOneTFSServerConfig.Tests/ConfigurationSerializerSpecs.cs
+++ VersionOneTFSServerConfig.Tests/ConfigurationSerializerSpecs.cs
@@ -13,12 +13,33 @@ namespace VersionOneTFSServerConfig.Tests
 
             context["when i serialize the configuration to json"] = () =>
                 {
-                    it["then the json is valid and ready to be sent to the service"] = () =>
+                    it["then the json is valid"] = () =>
                         {
 
                         };
                 };
 
         }
+
+        public void given_configuration_is_being_deserialized()
+        {
+            before = () =>
+                {
+
+                };
+
+            context["when i deserialize the configuration from json"] = () =>
+                {
+                    it["then a valid .net object results"] = () =>
+                        {
+
+                        };
+                };
+
+        }
+
     }
+
+    
+
 }
