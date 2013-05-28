8117369cc85e8ca627171b4915c82c3a99cfb071
codeEmitter <greg.johnson@versionone.com>
2013-05-28 12:27:53 -0400

    Setup test to expose content length issue.

--- VersionOneTFSServer.Tests/ServiceSpecs.cs
+++ VersionOneTFSServer.Tests/ServiceSpecs.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.ServiceModel;
+using System.Xml;
 using NSpec;
 
 namespace VersionOneTFSServer.Tests
 {
+
     public class ServiceSpecs : nspec
     {
 
@@ -22,6 +25,30 @@ namespace VersionOneTFSServer.Tests
                     }
 
                 };
+        }
+
+        public void given_a_soap_message_with_a_string_length_greater_than_the_default_of_8192_is_sent()
+        {
+
+            var longXml = GetLongXmlString();
+
+            it["then the message is processed successfully"] = () =>
+                {
+                    var client = new ServiceProxy.ServiceClient();
+                    client.Notify(longXml, "<TeamFoundationServer url='http://localhost:8080/tfs/DefaultCollection/Services/v3.0/LocationService.asmx' />");
+                };
+
+
+        }
+
+        private string GetLongXmlString()
+        {
+            var doc = new XmlDocument();
+            doc.Load(@"Resources/LongXML.xml");
+            var stringWriter = new StringWriter();
+            var xmlTextWriter = new XmlTextWriter(stringWriter);
+            doc.WriteTo(xmlTextWriter);
+            return stringWriter.ToString();
         }
 
     }
