4d38d94d4de0ae8f5da81720e833a95a0b7ecbd9
codeEmitter <greg.johnson@versionone.com>
2013-05-28 16:19:54 -0400

    Add test and sample data for a TFS build event.

--- VersionOneTFSServer.Tests/ServiceSpecs.cs
+++ VersionOneTFSServer.Tests/ServiceSpecs.cs
@@ -30,7 +30,7 @@ namespace VersionOneTFSServer.Tests
         public void given_a_soap_message_with_a_string_length_greater_than_the_default_of_8192_is_sent()
         {
 
-            var longXml = GetLongXmlString();
+            var longXml = GetXmlString(@".\Resources\LongCheckinEvent.xml");
 
             it["then the message is processed successfully"] = () =>
                 {
@@ -49,10 +49,32 @@ namespace VersionOneTFSServer.Tests
 
         }
 
-        private static string GetLongXmlString()
+        /// <summary>
+        /// Commented out due to external deps on a TFS server being present.  Left in place for its usefulness in debugging customer reported exceptions.
+        /// </summary>
+        //public void given_a_soap_message_for_a_build_event()
+        //{
+        //    var buildXml = GetXmlString(@".\Resources\BuildEventExample.xml");
+
+        //    it["then the message is processed successfully"] = () =>
+        //        {
+        //            var client = new ServiceProxy.ServiceClient();
+        //            try
+        //            {
+        //                client.Notify(buildXml, "<TeamFoundationServer url='http://localhost:8080/tfs/DefaultCollection/Services/v3.0/LocationService.asmx' />");
+        //            }
+        //            finally
+        //            {
+        //                if (client.State != CommunicationState.Closed) client.Close();
+        //            }
+        //        };
+
+        //}
+
+        private static string GetXmlString(string path)
         {
             var doc = new XmlDocument();
-            doc.Load(@".\Resources\LongXML.xml");
+            doc.Load(path);
             var stringWriter = new StringWriter();
             var xmlTextWriter = new XmlTextWriter(stringWriter);
             doc.WriteTo(xmlTextWriter);
