1cf169a3b159ef92ae21e45d1c3947ca128844ee
Ian Buchanan <ian.buchanan@versionone.com>
2014-01-13 18:08:06 -0500

    fixing build and dependencies

--- build.sh
+++ build.sh
@@ -1,7 +1,36 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+set -ex
+## x = exit immediately if a pipeline returns a non-zero status.
+## e = print a trace of commands and their arguments during execution.
+## See: http://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html#The-Set-Builtin
+
+# ----- Variables -------------------------------------------------------------
+# Variables in the build.properties file will be available to Jenkins
+# build steps. Variables local to this script can be defined below.
 . ./build.properties
-if [ -d build-tools ]; then cd build-tools && git fetch && git stash && git pull && cd ..; else git clone https://github.com/versionone/openAgile-build-tools.git build-tools; fi
-source ./build-tools/common.sh
+
+# fix for jenkins inserting the windows-style path in $WORKSPACE
+cd "$WORKSPACE"
+export WORKSPACE=`pwd`
+
+
+
+# ----- Common ----------------------------------------------------------------
+# Common build script creates functions and variables expected by Jenkins.
+if [ -d $WORKSPACE/../build-tools ]; then
+  ## When script directory already exists, just update when there are changes.
+  cd $WORKSPACE/../build-tools
+  git fetch && git stash
+  if ! git log HEAD..origin/master --oneline --quiet; then
+    git pull
+  fi
+  cd $WORKSPACE
+else
+  git clone https://github.com/versionone/openAgile-build-tools.git $WORKSPACE/../build-tools
+fi
+source ../build-tools/common.sh
+
+
 
 # ---- Produce .NET Metadata --------------------------------------------------
 
@@ -31,15 +60,26 @@ EOF
 done
 
 
+
 # ---- Clean solution ---------------------------------------------------------
 
-# MSBuild.exe $SOLUTION_FILE /m /t:Clean
+MSBuild.exe $SOLUTION_FILE -m \
+  -t:Clean \
+  -p:Configuration="$Configuration" \
+  -p:Platform="$Platform" \
+  -p:Verbosity=Diagnostic
+
+
+
+
+# ---- Refresh nuget packages -------------------------------------------------
 
 nuget_packages_refresh
 
+
+
 # ---- Build solution using msbuild -------------------------------------------
 
-WIN_SIGNING_KEY="`winpath "$SIGNING_KEY"`"
 WIN_SIGNING_KEY="`winpath "$SIGNING_KEY"`"
 echo $Platform
 MSBuild.exe $SOLUTION_FILE \
@@ -48,9 +88,12 @@ MSBuild.exe $SOLUTION_FILE \
   -p:RequireRestoreConsent=false \
   -p:Configuration="$Configuration" \
   -p:Platform="$Platform" \
-  -p:Verbosity=Diagnostic
+  -p:Verbosity=Diagnostic \
+  -p:VisualStudioVersion=11.0
 
 
 # ---- Execute nspec tests -------------------------------------------
 
 ./tests.sh
+
+
